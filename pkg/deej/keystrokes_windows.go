f27b5218a2d0f551fb00c087a1a755801d4ceda2
Moon-2006 <90346045+Moon-2006@users.noreply.github.com>
2026-03-16 21:59:29 -0400

    Refactor sendKeystrokes to use custom input types

--- pkg/deej/keystrokes_windows.go
+++ pkg/deej/keystrokes_windows.go
@@ -4,55 +4,84 @@
 package deej
 
 import (
-        "fmt"
-        "unsafe"
-
-        "golang.org/x/sys/windows"
+	"fmt"
+	"syscall"
+	"unsafe"
 )
 
-// sendKeystrokes dispatches one or more virtual key codes as a single key
-// sequence using the Windows SendInput API.  It presses each key in order
-// and then releases them in reverse order.  If any error occurs the
-// returned error will be non‑nil.
+const (
+	inputKeyboard   = 1
+	keyeventfKeyUp  = 0x0002
+)
+
+type keyboardInput struct {
+	Vk         uint16
+	Scan       uint16
+	Flags      uint32
+	Time       uint32
+	ExtraInfo  uintptr
+}
+
+type input struct {
+	Type uint32
+	Ki   keyboardInput
+	// The real Win32 INPUT union is larger, but for keyboard input this is
+	// enough for the old Go/Windows build used by this repo.
+}
+
+var (
+	user32         = syscall.NewLazyDLL("user32.dll")
+	procSendInput  = user32.NewProc("SendInput")
+)
+
 func sendKeystrokes(keys []uint16) error {
-        if len(keys) == 0 {
-                return nil
-        }
-        // Build input slice: first keydown events, then keyup events in reverse
-        inputs := make([]windows.INPUT, 0, len(keys)*2)
-        for _, vk := range keys {
-                ki := windows.KEYBDINPUT{
-                        Vk:         vk,
-                        Scan:       0,
-                        Flags:      0,
-                        Time:       0,
-                        ExtraInfo:  0,
-                }
-                inputs = append(inputs, windows.INPUT{
-                        Type: windows.INPUT_KEYBOARD,
-                        Ki:   ki,
-                })
-        }
-        for i := len(keys) - 1; i >= 0; i-- {
-                vk := keys[i]
-                ki := windows.KEYBDINPUT{
-                        Vk:         vk,
-                        Scan:       0,
-                        Flags:      windows.KEYEVENTF_KEYUP,
-                        Time:       0,
-                        ExtraInfo:  0,
-                }
-                inputs = append(inputs, windows.INPUT{
-                        Type: windows.INPUT_KEYBOARD,
-                        Ki:   ki,
-                })
-        }
-        sent, err := windows.SendInput(uint32(len(inputs)), &inputs[0], int32(unsafe.Sizeof(inputs[0])))
-        if err != nil {
-                return err
-        }
-        if sent != uint32(len(inputs)) {
-                return fmt.Errorf("SendInput sent %d of %d events", sent, len(inputs))
-        }
-        return nil
+	if len(keys) == 0 {
+		return nil
+	}
+
+	inputs := make([]input, 0, len(keys)*2)
+
+	// key down
+	for _, vk := range keys {
+		inputs = append(inputs, input{
+			Type: inputKeyboard,
+			Ki: keyboardInput{
+				Vk:        vk,
+				Scan:      0,
+				Flags:     0,
+				Time:      0,
+				ExtraInfo: 0,
+			},
+		})
+	}
+
+	// key up in reverse order
+	for i := len(keys) - 1; i >= 0; i-- {
+		vk := keys[i]
+		inputs = append(inputs, input{
+			Type: inputKeyboard,
+			Ki: keyboardInput{
+				Vk:        vk,
+				Scan:      0,
+				Flags:     keyeventfKeyUp,
+				Time:      0,
+				ExtraInfo: 0,
+			},
+		})
+	}
+
+	ret, _, err := procSendInput.Call(
+		uintptr(len(inputs)),
+		uintptr(unsafe.Pointer(&inputs[0])),
+		unsafe.Sizeof(inputs[0]),
+	)
+
+	if ret == 0 {
+		if err != syscall.Errno(0) {
+			return err
+		}
+		return fmt.Errorf("SendInput failed")
+	}
+
+	return nil
 }
